851b9f2b1099d07d7e42f28d80dfdea7598dd47a
Stefan Buhrmester <buhrmi@gmail.com>
2022-07-18 17:57:49 +0200

    logo

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<slot/>
+
+<style>
+  :global(body) {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: white;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background: linear-gradient(
+      to bottom,
+      rgba(10, 10, 30),
+      rgba(5, 5, 20)
+    );
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+
+
+<img src="$lib/images/logo.png" alt="CDMN" />
+
+<p>
+  Mutter-Ey-Platz 1<br>
+  40213 Düsseldorf<br>
+  Tel: +49 211 863 230 260<br>
+  Mail: info@cdmn.de
+</p>
+<style>
+  img {
+    width: 50%;
+  }
+</style>
